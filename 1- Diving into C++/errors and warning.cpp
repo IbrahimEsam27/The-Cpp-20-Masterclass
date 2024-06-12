7a57fdd7673d791aefb360df052413cf2c7866ff
Ibrahim Esam Ibrahim <ibrahimesam170@gmail.com>
2024-06-13 02:58:36 +0300

    errors and warning.cpp

--- 1- Diving into C++/errors and warning.cpp
+++ 1- Diving into C++/errors and warning.cpp
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
 {
     std::cout << "Hello from MasterClass C++20 " << std::endl;
 
-    //Gives a Warning, it will compile with no problems
+    //Gives a Warning, it will compile with no problems 
     int value = 7 / 0;
     // runtime error the program craches at this Line
     std::cout << "Value = " << value << std::endl;
