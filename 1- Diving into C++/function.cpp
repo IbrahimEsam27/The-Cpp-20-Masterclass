52ba81bf49d2b00cc1fbffc7cdbeb062faed1ce4
Ibrahim Esam Ibrahim <ibrahimesam170@gmail.com>
2024-06-13 21:34:34 +0300

    function.cpp

--- 1- Diving into C++/function.cpp
+++ 1- Diving into C++/function.cpp
@@ -7,9 +7,9 @@ int AddNumber(int number1, int number2)
 
 int main(int argc, char const *argv[])
 {
-    int number1= 13, number2{15}; // Assignment with {} shoul be the same type of int cannot be float
-    float number3 {15.5};
-    std::cout << "Sum of Two numbers = " << AddNumber(number1, number2) << std::endl; 
+    int number1 = 13, number2{15}; // Assignment with {} shoul be the same type of int cannot be float
+    float number3{15.5};
+    std::cout << "Sum of Two numbers = " << AddNumber(number1, number2) << std::endl;
 
     return 0;
 }
